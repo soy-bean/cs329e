79c40aacbea6a0e63aa714862541cea01c591c3a
carlos-loya <exsorest@gmail.com>
2017-10-26 01:56:52 -0500

    Revamped html files with jinja template inheritance and fixed corresponding href tags. Updated canja.py and test.py to reflect changes. Updated folder structure to match teacher template. closes #3, closes #8

--- test.py
+++ test.py
@@ -15,7 +15,43 @@ class MyTestClass(unittest.TestCase):
         self.assertEqual(result.status_code, 200)
 
     def test_stats_status_code(self):
-        result = self.app.get('/stats/')
+        result = self.app.get('/about/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_book1_status_code(self):
+        result = self.app.get('/book/1')
+        self.assertEqual(result.status_code, 200)
+
+    def test_book2_status_code(self):
+        result = self.app.get('/book/2')
+        self.assertEqual(result.status_code, 200)
+
+    def test_book3_status_code(self):
+        result = self.app.get('/book/3')
+        self.assertEqual(result.status_code, 200)
+
+    def test_author1_status_code(self):
+        result = self.app.get('/author/1')
+        self.assertEqual(result.status_code, 200)
+
+    def test_author2_status_code(self):
+        result = self.app.get('/author/2')
+        self.assertEqual(result.status_code, 200)
+
+    def test_author3_status_code(self):
+        result = self.app.get('/author/3')
+        self.assertEqual(result.status_code, 200)
+
+    def test_publisher1_status_code(self):
+        result = self.app.get('/publisher/1')
+        self.assertEqual(result.status_code, 200)
+
+    def test_publisher2_status_code(self):
+        result = self.app.get('/publisher/2')
+        self.assertEqual(result.status_code, 200)
+
+    def test_publisher3_status_code(self):
+        result = self.app.get('/publisher/3')
         self.assertEqual(result.status_code, 200)
 
     def test_not_found_status_code(self):
