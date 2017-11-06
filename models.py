5b94d5849f3f9a036faf9a4a7c34852332b3546b
Andrew Chen <aychen@utexas.edu>
2017-11-06 13:51:24 -0600

    update DDL for models.py

--- models.py
+++ models.py
@@ -8,25 +8,50 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-# Book_Author_Junction = Table('book', Base.metadata,
-#     Column('author_id', Integer, ForeignKey('authors.id')),
-#     Column('publisher_id', Integer, ForeignKey('publishers.id'))
-# )
-#
-# class Author(Base):
-#     __tablename__ = 'authors'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(80), nullable=False)
-#
-# class Publisher(Base):
-#     __tablename__ = 'publishers'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(80), nullable=False)
+Book_Author_Junction = Table('book_author', Base.metadata,
+    Column('author_id', Integer, ForeignKey('authors.id')),
+    Column('book_id', Integer, ForeignKey('book.id'))
+)
+
+Author_Publisher_Junction = Table('author_publisher', Base.metadata,
+    Column('author_id', Integer, ForeignKey('authors.id')),
+    Column('publisher_id', Integer, ForeignKey('publisher.id'))
+)
+
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    birth = Column(String(80))
+    education = Column(String(80))
+    nationality = Column(String(80))
+    alma_mater = Column(String(80))
+    wiki_link = Column(String(80))
+    image_url = Column(String(80))
+
+class Publisher(Base):
+    __tablename__ = 'publishers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    wiki_link = Column(String(80))
+    description = Column(String('max'))
+    owner = Column(String(80))
+    image_url = Column(String(80))
+    website = Column(String(80))
 
 class Book(Base):
     __tablename__ = 'book'
     id = Column(Integer, primary_key=True)
     title = Column(String(80), nullable=False)
+    google_id = Column(String(80), nullable=False)
+    isbn = Column(String(80), nullable=False)
+    pub_date = Column(String(80))
+    image_url = Column(String(80))
+    description = Column(String('max'))
+    publisher_id = Column(Integer, ForeignKey('publishers.id'))
+    publisher = relationship("Publisher")
+
+
 
 # Change postgresql://postgres:asd123@localhost/---->postgres<---- to the name of the database you give to your local system
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://postgres:asd123@localhost/postgres')
